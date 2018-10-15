3c15818d5264131614577862d0d716c1837a5c90
Akira Taniguchi <a.taniguchi@techbureau.jp>
2018-10-15 18:01:13 +0900

    まだ途中

--- currency.go
+++ currency.go
@@ -1,16 +1,16 @@
 package eth_utils
 
 import (
+	"fmt"
 	"errors"
-	"math"
-	"reflect"
+	//"reflect"
 )
 
 const MinWei = 0
 // 2の256乗-1
 const MaxWei = 115792089237316195423570985008687907853269984665640564039457584007913129639935
 
-var ethUnit = map[string]uint64 {
+var ethUnit = map[string]float64 {
     "wei":          1,
     "kwei":         1000,
     "babbage":      1000,
@@ -38,16 +38,20 @@ var ethUnit = map[string]uint64 {
 
 
 func FromWei(number uint64, unit string) (float64, error) {
-	ethUnitValue, ok = ethUnit[unit]
-	if ok == false {
-		keys := reflect.ValueOf(ethUnit).MapKeys()
-		return 0, errors.New(fmt.Printf("Unknown unit.  Must be one of %s", keys))
-	}
-	if number == 0 {
-		return 0, nil
-	}
-	if number < MinWei || number > MaxWei{
-		return 0, errors.New(fmt.Printf("value must be between 1 and %s", MaxWei))
-	}
-	return uint64 / ethUnitValue, nil
+	var v = ethUnit[unit]
+	fmt.Printf("Unknown unit.  Must be one of %f", v)
+	return 0, errors.New("test")
+	//ethUnitValue, exist := ethUnit[unit]
+	//if exist == false {
+	//	keys := reflect.ValueOf(ethUnit).MapKeys()
+	//	return 0, errors.New(fmt.Printf("Unknown unit.  Must be one of %s", keys))
+	//}
+	//return 0, errors.New("test")
+	//if number == 0 {
+	//	return 0, nil
+	//}
+	//if number < MinWei || number > MaxWei{
+	//	return 0, errors.New(fmt.Printf("value must be between 1 and %s", MaxWei))
+	//}
+	//return float64(uint64) / float64(ethUnitValue), nil
 }
--- currency_test.go
+++ currency_test.go
@@ -1,14 +1,15 @@
 package eth_utils
 
 import (
+	//"fmt"
 	"testing"
-	"eth-utils"
 )
 
 func Test_From_Wei_1(t *testing.T) {
-	w, e := FromWei(1, "ether")
-	if e != nil{
-		t.Error("除算関数のテストが通りません") // もし予定されたものでなければエラーを発生させます。
-	}
-	fmt.Printf("", w)
+	FromWei(1, "tether")
+	//w, e := FromWei(1, "ether")
+	//if e != nil{
+	//	t.Error("除算関数のテストが通りません") // もし予定されたものでなければエラーを発生させます。
+	//}
+	//fmt.Printf("", w)
 }
