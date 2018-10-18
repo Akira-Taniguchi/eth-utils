50f6b31d0cd405ac8e13c7bfc31c2ca8e46674e9
EC2 Default User <ec2-user@ip-172-31-15-45.us-east-2.compute.internal>
2018-10-18 10:46:53 +0000

    Is0xPrefixedとAdd0xPrefixの実装

--- hexadecimal.go
+++ hexadecimal.go
@@ -3,8 +3,19 @@ package eth_utils
 import "strings"
 
 func Remove0xPrefix(value string) string {
-	if strings.HasPrefix(strings.ToUpper(value), "0X") == false {
+	if Is0xPrefixed(value) == false {
 		return value
 	}
 	return string([]rune(value)[2:])
+}
+
+func Is0xPrefixed(value string) bool {
+	return strings.HasPrefix(strings.ToLower(value), "0x")
+}
+
+func Add0xPrefix(value string) string {
+	if Is0xPrefixed(value) {
+		return value
+	}
+	return "0x" + value
 }
--- hexadecimal_test.go
+++ hexadecimal_test.go
@@ -17,3 +17,38 @@ func Test_Remove0xPrefix_2(t *testing.T) {
 		t.Error("error")
 	}
 }
+
+func Test_Is0xPrefixed_1(t *testing.T) {
+	v := Is0xPrefixed("e89943Ec20d856F064A87349a00Ef6aB00AED042")
+	if v {
+		t.Error("error")
+	}
+}
+
+func Test_Is0xPrefixed_2(t *testing.T) {
+	v := Is0xPrefixed("0xe89943Ec20d856F064A87349a00Ef6aB00AED042")
+	if v == false {
+		t.Error("error")
+	}
+}
+
+func Test_Is0xPrefixed_3(t *testing.T) {
+	v := Is0xPrefixed("0Xe89943Ec20d856F064A87349a00Ef6aB00AED042")
+	if v == false {
+		t.Error("error")
+	}
+}
+
+func Test_Add0xPrefix_1(t *testing.T) {
+	v := Add0xPrefix("e89943Ec20d856F064A87349a00Ef6aB00AED042")
+	if v != "0xe89943Ec20d856F064A87349a00Ef6aB00AED042" {
+		t.Error("error")
+	}
+}
+
+func Test_Add0xPrefix_2(t *testing.T) {
+	v := Add0xPrefix("0xe89943Ec20d856F064A87349a00Ef6aB00AED042")
+	if v != "0xe89943Ec20d856F064A87349a00Ef6aB00AED042" {
+		t.Error("error")
+	}
+}
